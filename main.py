976ca4973de519ca51034c96568eb37e3cd612df
SamraAzizi <samarraazizi@gmail.com>
2025-05-13 18:22:16 +0500

    resume critiquer .env

---
+++ main.py
@@ -0,0 +1,10 @@
+import streamli as st
+import PyPDF2
+import io
+import os
+from openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+st.set_page_config(page_title="AI Resume Critiquer", page_icon="📃", layout="centered")
