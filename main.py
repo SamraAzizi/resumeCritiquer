5fb6456d4a2e5ed07a2b93632cc0f057f7a954fc
SamraAzizi <samarraazizi@gmail.com>
2025-05-14 11:52:53 +0500

    resume critiquer .env

--- main.py
+++ main.py
@@ -8,3 +8,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 st.set_page_config(page_title="AI Resume Critiquer", page_icon="📃", layout="centered")
+
+st.title("AI Resume Critiquer")
+st.markdown("Upload your resume and get AI-powered feedbac tailored to your needs!")
+
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+uploaded_file = st.file_uploader("Upload Your resume (PDF or TXT)", type=["pdf", "txt"])
